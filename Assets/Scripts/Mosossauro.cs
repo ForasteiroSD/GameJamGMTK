4f86b026a0cd2d9c5f8f0eadd1b132e9e2b78d1a
Kamal <91926257+KamalFR@users.noreply.github.com>
2023-07-08 22:21:46 -0300

    codigos

--- Assets/Scripts/Mosossauro.cs
+++ Assets/Scripts/Mosossauro.cs
@@ -8,6 +8,7 @@ public class Mosossauro : MonoBehaviour
 {
     [SerializeField] private float tamanhoPulo;
     [SerializeField] private Transform jogador;
+    [SerializeField] private Porrete defende;
     private Vector3 lugar;
     private Boolean atacou;
 
@@ -32,13 +33,27 @@ public class Mosossauro : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(((jogador.position.x - lugar.x) < 1)&&(!atacou))
+        if(((jogador.position.x - lugar.x)> -1)&&(!atacou))
         {
             ataque();
         }
-        if(((jogador.position.x - lugar.x) < -1) && (atacou))
+        if(((jogador.position.x - lugar.x) > 1) && (atacou))
         {
             volta();
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            if (defende.invulneravel) 
+            {
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                Debug.Log("TomeiDano");
+            }
         }
     }
 }
