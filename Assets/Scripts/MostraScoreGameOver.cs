803e947b489903db3d5e97cb37fce19a33ad8872
Joao20037 <joaolucas.moraes.oliveira@gmail.com>
2023-07-09 03:09:50 -0300

    GameOver tela

--- Assets/Scripts/MostraScoreGameOver.cs
+++ Assets/Scripts/MostraScoreGameOver.cs
@@ -6,12 +6,12 @@ using TMPro;
 public class MostraScoreGameOver : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _score;
-    [SerializeField] private GameObject _scoreHandler;
+    //[SerializeField] private GameObject _scoreHandler;
     private float _actualScore;
     // Start is called before the first frame update
     void Start()
     {
-        _actualScore = _scoreHandler.GetComponent<DistanciaPercorrida>().getScore();
+        _actualScore = GetComponent<DistanciaPercorrida>().getScore();
         _score.text = _actualScore.ToString();
     }
 
